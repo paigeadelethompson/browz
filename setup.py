c36bf4fc5d14fb28c7391aa2209aef5d073ee3b8
Paige <paige@paige.bio>
2023-02-25 01:30:17 -0800

    long desc and site url

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     version='1.0.1',
     description='Python/GTK Web Browser',
     author='Millan Philipose',
+    url='https://github.com/paigeadelethompson/browz',
     license='MIT',
+    long_description='A formerly abandoned Python and GTK web browser written by Millan Philipose',
     install_requires=[
         'autopep8==2.0.1',
         'distutils-extra-python==2.39.2',
